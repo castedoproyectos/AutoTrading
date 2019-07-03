09836a05249265fba61c58b795fd81fc20af5f7c
Carlos Castedo <castedo.proyectos@gmail.com>
2019-07-04 00:44:21 +0200

    Clean telegramhandler

--- telegramhandler.py
+++ telegramhandler.py
@@ -6,7 +6,6 @@ import time
 class TelegramHandler(object):
 
     def __init__(self):
-
         self._list_msn = list()
 
         self._sesion_name = "test"
@@ -23,20 +22,36 @@ class TelegramHandler(object):
 
 
     def new_msn(self, list_msn):
-
+        """Realizael filtro de los mensajes para poder identificar cuales de 
+        ellos son nuevos para el sistema.
+        
+        Arguments:
+            list_msn {[list]} -- Lista de mensajes recibidos para ser filtrados
+        
+        Returns:
+            {[list]} -- Devuelve la lista de mensajes ya filtrada y sin los repetidos.
+        """
         while len(list_msn) > 0: 
             if list_msn[0] in self._list_msn:
                 list_msn.pop(0)        
             else:
                 break
         
+        self._list_msn.extend(list_msn)
         return list_msn
 
 
-    def get_msn(self):
-
-        messages = list()
-
+    def get_msn(self, num_msn):
+        """Obtiene los mensajes de un canal de telegram, fijado como variable
+        interna de la clase (self._channel_entity). Los mensajes estan colocados
+        de forma que el primero es el que menos tiempo tiene desde su envio.
+        
+        Arguments:
+            num_msn {[int]} -- Número de mensajes a obtener en el canal.
+        
+        Returns:
+            [list] -- Devuelve una lista de mensajes, leidos y no leidos.
+        """
         posts = self._client(GetHistoryRequest(
             peer=self._channel_entity,
             limit=100,
@@ -47,7 +62,18 @@ class TelegramHandler(object):
             add_offset=0,
             hash=0))
     
-        for it in enumerate(posts.messages):
-            messages.append(posts.messages[it].message)
-
+        # TODO Ordenarlo al contrario. 0 -> n-1
+        return list(posts.messages[:].message)
         
+        
+    def main_loop(self, num_msn, q):
+        """Bucle de ejecución principal donde se adquieren los mensajes,
+        se filtran para identificar los buenos y se colocan en una cola.
+        
+        Arguments:
+            num_msn {[int]} -- Número de mensajes a coger en cada iteración
+            q {[queue]} -- Cola de mensajes donde situarlos
+        """
+        msn = self.get_msn(num_msn)
+        new_msn = self.new_msn(msn)
+        q.put(new_msn)
