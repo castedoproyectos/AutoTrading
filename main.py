70025515d36e25a3db11f71a82a004d1281d75dc
Carlos Castedo <carlos.castedo@alten.es>
2019-07-04 08:12:57 +0200

    Inicio multithread

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
 
+import threading, queue
+
+def func1(num, q):
+    while num < 100000000:
+        num = num**2
+
+        a = ['hola', 'carlos']
+        q.put(a)
+
+def func2(num, q):
+    while num < 100000000:
+        lista = q.get()
+        print(num)
+
+num = 2
+q = queue.Queue()
+thread1 = threading.Thread(target=func1,args=(num,q))
+thread2 = threading.Thread(target=func2,args=(num,q))
+
+print('setup')
+
+thread1.start()
+thread2.start()
